163953984b0755a0627d411ad98ffd4e5a701ea1
Jhonny <jhonnycgarcia@gmail.com>
2024-10-22 23:46:26 -0500

    Confuguraciones iniciales del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MapsModule } from './maps/maps.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MapsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-map-screen',
+  templateUrl: './map-screen.component.html',
+  styleUrl: './map-screen.component.css'
+})
+export class MapScreenComponent {
+
+}
